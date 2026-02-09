c4d0e1f2eb872e2d3242424a7c5f1639321b5f44
quiclane <developer@veridanlayerseven.com>
2026-02-09 21:48:55 +0200

    fix checksum for 1.0.1

--- Package.swift
+++ Package.swift
@@ -1,3 +1,3 @@
 // swift-tools-version:6.0
 import PackageDescription
-let package=Package(name:"NGHTTP3",platforms:[.iOS(.v13)],products:[.library(name:"NGHTTP3",targets:["NGHTTP3"])],targets:[.binaryTarget(name:"NGHTTP3",url:"https://github.com/quiclane/nghttp3-spm/releases/download/1.0.2/NGHTTP3.xcframework.zip",checksum:"33e7427d8989f5ba194171cdaa7af3635073acd521fda48324668ad5eec09a41")])
+let package=Package(name:"NGHTTP3",platforms:[.iOS(.v13)],products:[.library(name:"NGHTTP3",targets:["NGHTTP3"])],targets:[.binaryTarget(name:"NGHTTP3",url:"",checksum:"")])
