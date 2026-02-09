261164cd110c314e335b2b61ba15690ecbff4e01
quiclane <developer@veridanlayerseven.com>
2026-02-09 22:10:07 +0200

    spm 1.0.6

--- Package.swift
+++ Package.swift
@@ -1,3 +1,3 @@
 // swift-tools-version:6.0
 import PackageDescription
-let package=Package(name:"NGHTTP3",platforms:[.iOS(.v13)],products:[.library(name:"NGHTTP3",targets:["NGHTTP3"])],targets:[.binaryTarget(name:"NGHTTP3",url:"https://github.com/quiclane/nghttp3-spm/releases/download/1.0.5/NGHTTP3.xcframework.zip",checksum:"0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")])
+let package=Package(name:"NGHTTP3",platforms:[.iOS(.v13)],products:[.library(name:"NGHTTP3",targets:["NGHTTP3"])],targets:[.binaryTarget(name:"NGHTTP3",url:"https://github.com/quiclane/nghttp3-spm/releases/download/1.0.6/NGHTTP3.xcframework.zip",checksum:"a97ad538f4ab96766b9e0ad6eb278eb7c40677b16181e63a241a2cd792c3ac1c")])
